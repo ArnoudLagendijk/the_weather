f5e016c4db80c9d0061d020d296fa9c70d51a6fb
Arnoud Lagendijk <arnoud.lagendijk@kpn.com>
2021-04-25 20:10:14 +0200

    added model

--- weather/views.py
+++ weather/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 from requests import get
 
+from .models import City
+
 api_key = "e5fee864aec066e146ee446fb6e64407"
 
 
 def index(request, *args, **kwargs):
-    city = "Zoetermeer"
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
-    r = get(url)
-    print(r.text)
-    return render(request, 'weather/weather.html')
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+        r = get(url).json()
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon']
+        }
+        weather_data.append(city_weather)
+
+    print(weather_data)
+
+    context = {'weather_data': weather_data}
+
+    return render(request, 'weather/weather.html', context)
