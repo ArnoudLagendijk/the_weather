01d4163c414b2843c781a33d468f1f1ae96ca9e0
Arnoud Lagendijk <arnoud.lagendijk@kpn.com>
2021-04-25 17:27:27 +0200

    build_request_to_weather

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from requests import get
+
+api_key = "e5fee864aec066e146ee446fb6e64407"
+
 
 def index(request, *args, **kwargs):
+    city = "Zoetermeer"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    r = get(url)
+    print(r.text)
     return render(request, 'weather/weather.html')
